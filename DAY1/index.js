30d9cdf594526d0de52f6e96eba50b26c2cc470c
ACora Hickson <ahickson25@amherst.edu>
2022-07-16 08:17:28 -0400

    introduction website

---
+++ DAY1/index.js
@@ -0,0 +1,11 @@
+document.getElementById("btn").addEventListener("mouseover", function(){
+    document.getElementById("btn").style.backgroundColor = "orange";
+
+});
+
+document.getElementById("btn").addEventListener("mouseout", function(){
+        document.getElementById("btn").style.backgroundColor = "white";
+});
+
+
+
